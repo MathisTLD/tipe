4341d43c1063fa90b6e644e6c18b3598ce61b307
MathisTLD <mathis.tailland@gmail.com>
2021-06-06 19:58:32 +0200

    add heuristic tests

--- public/api/calculator.router.js
+++ public/api/calculator.router.js
@@ -9,6 +9,12 @@ module.exports = {
         .then((results) => res.json(results))
         .catch(() => res.sendStatus(400));
     });
+    this.router.get("/heuristic-tests", bodyparser.json(), (req, res) => {
+      this.$app.calculator
+        .getHeuristicTestResults()
+        .then((results) => res.json(results))
+        .catch(() => res.sendStatus(500));
+    });
     this.router.post("/run", bodyparser.json(), (req, res) => {
       const opts = req.body;
       this.$app.calculator
